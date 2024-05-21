134fd1bd58da5842a0c33cd6cb82a5d0c262729b
Pedro Noronha <pedro_noronha@outlook.com>
2024-05-21 12:33:00 -0300

    finalizando

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente Post", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<Post />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve adicionar dois comentários", () => {
+    render(<Post />);
+
+    //teste
+    fireEvent.change(screen.getByTestId("campoComentario"), {
+      target: {
+        value: "teste",
+      },
     });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    // teste2
+    fireEvent.change(screen.getByTestId("campoComentario"), {
+      target: {
+        value: "teste2",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comment-button"));
+
+    expect(screen.getAllByTestId("comentarioAdicionado")).toHaveLength(2);
+  });
 });
